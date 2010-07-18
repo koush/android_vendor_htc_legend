19725f65ecffd12a0cafa65695308d05243504f4
Koushik Dutta <koushd@gmail.com>
2010-07-17 17:39:31 -0700

    auto vs ext3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,5 +18,3 @@ BOARD_SYSTEMIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x07500000)
 BOARD_USERDATAIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x04ac0000)
 # The size of a block that can be marked bad.
 BOARD_FLASH_BLOCK_SIZE := 131072
-
-BOARD_SDEXT_FILESYSTEM := ext3
